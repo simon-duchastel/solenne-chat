066abf2c65b89def6e64579a8af0177be45dbd4b
Simon Duchastel <simon@duchastel.com>
2025-04-16 21:30:31 -0400

    Add comment, clean up code

--- composeApp/src/commonMain/kotlin/com/duchastel/simon/solenne/parcel/Parcelize.kt
+++ composeApp/src/commonMain/kotlin/com/duchastel/simon/solenne/parcel/Parcelize.kt
@@ -1,5 +1,10 @@
 package com.duchastel.simon.solenne.parcel
 
+/**
+ * This is necessary for Circuit to work on Android, since CircuitScreens
+ * are expected to have an Android `Parcelize` annotation.
+ * We use the `additionalAnnotation` Parcelize compiler flag to make this work.
+ */
 @OptIn(ExperimentalMultiplatform::class)
 @OptionalExpectation
 @Target(AnnotationTarget.CLASS)
--- composeApp/src/commonMain/kotlin/com/duchastel/simon/solenne/screens/chat/ChatPresenter.kt
+++ composeApp/src/commonMain/kotlin/com/duchastel/simon/solenne/screens/chat/ChatPresenter.kt
@@ -3,22 +3,14 @@ package com.duchastel.simon.solenne.screens.chat
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import com.duchastel.simon.solenne.data.ChatMessage
 import com.duchastel.simon.solenne.data.ChatMessageRepository
-import com.duchastel.simon.solenne.ui.model.UIChatMessage
 import com.duchastel.simon.solenne.ui.model.toUIChatMessage
 import com.slack.circuit.runtime.presenter.Presenter
 import dev.zacsweers.metro.Assisted
 import dev.zacsweers.metro.AssistedFactory
 import dev.zacsweers.metro.Inject
-import kotlinx.collections.immutable.PersistentList
-import kotlinx.collections.immutable.mutate
-import kotlinx.collections.immutable.persistentListOf
-import kotlinx.collections.immutable.toImmutableList
 import kotlinx.collections.immutable.toPersistentList
-import kotlinx.coroutines.flow.map
 
 class ChatPresenter @Inject constructor(
     private val repository: ChatMessageRepository,
