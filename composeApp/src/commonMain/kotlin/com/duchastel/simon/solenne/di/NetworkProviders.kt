b1608e5a3dfd8819f9fc44a57fd20a0b61ea43dd
Simon Duchastel <simon@duchastel.com>
2025-04-17 12:19:32 -0400

    use basic network logger

--- composeApp/src/commonMain/kotlin/com/duchastel/simon/solenne/di/NetworkProviders.kt
+++ composeApp/src/commonMain/kotlin/com/duchastel/simon/solenne/di/NetworkProviders.kt
@@ -3,7 +3,6 @@ package com.duchastel.simon.solenne.di
 import dev.zacsweers.metro.Provides
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
