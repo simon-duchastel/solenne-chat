c61c8dabf19a2d628a5837fe2325dc863fe4d97a
Simon Duchastel <simon@duchastel.com>
2025-05-17 01:27:07 -0400

    Update ModelScope

--- composeApp/src/commonMain/kotlin/com/duchastel/simon/solenne/data/ai/AiChatRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/com/duchastel/simon/solenne/data/ai/AiChatRepositoryImpl.kt
@@ -31,7 +31,10 @@ class AiChatRepositoryImpl @Inject constructor(
 ) : AiChatRepository {
 
     override fun getAvailableModelsFlow(): Flow<List<AIModelProviderStatus<*>>> {
-        return aiApiKeyDb.getGeminiModelScopeFlow()
+        return aiApiKeyDb.getGeminiApiKeyFlow()
+            .map { apiKey ->
+                if (apiKey == null) null else GeminiModelScope(apiKey)
+            }
             .map(GeminiModelScope?::toGeminiModelProviderStatus)
             .map(::listOf)
     }
