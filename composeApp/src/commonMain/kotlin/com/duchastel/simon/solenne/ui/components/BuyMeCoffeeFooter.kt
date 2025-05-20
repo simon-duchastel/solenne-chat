c09f15ef9bf203bdb45a5ceab46e3c8e57226aab
Simon Duchastel <simon@duchastel.com>
2025-05-20 00:03:16 -0400

    Tiny tweak to remove unnecessary color in buy me a coffee footer

--- composeApp/src/commonMain/kotlin/com/duchastel/simon/solenne/ui/components/BuyMeCoffeeFooter.kt
+++ composeApp/src/commonMain/kotlin/com/duchastel/simon/solenne/ui/components/BuyMeCoffeeFooter.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
@@ -15,9 +14,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.duchastel.simon.solenne.ui.icons.coffeeIcon
@@ -40,7 +36,6 @@ fun BuyMeCoffeeFooter(
             painter = coffeeIcon(),
             contentDescription = null,
             modifier = Modifier.size(24.dp),
-            tint = Color(0xFF333333)
         )
         Spacer(modifier = Modifier.weight(1f))
         Text(
