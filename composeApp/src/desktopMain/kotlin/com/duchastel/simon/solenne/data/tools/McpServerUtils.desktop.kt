89da994ffb386d3d2fbdc7eb8b34c7e76980a596
Simon Duchastel <simon@duchastel.com>
2025-05-26 15:07:20 -0400

    Remove debug code in desktop

--- composeApp/src/desktopMain/kotlin/com/duchastel/simon/solenne/data/tools/McpServerUtils.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/duchastel/simon/solenne/data/tools/McpServerUtils.desktop.kt
@@ -8,19 +8,12 @@ import kotlinx.io.buffered
 import kotlinx.io.bytestring.decodeToString
 import kotlinx.io.readByteString
 
-@OptIn(InternalIoApi::class)
 actual fun createStdioServerTransport(connection: McpServerConfig.Connection.Stdio): StdioServerTransport? {
     val process = ProcessBuilder(
         connection.commandToRun.split(" ")
     ).start()
     return StdioServerTransport(
-        inputStream = process.inputStream.asSource().buffered().also {
-            val peekBuffer = it.peek().buffer.copy()
-            println("Input Preview: " + peekBuffer.readByteString().decodeToString())
-        },
-        outputStream = process.outputStream.asSink().buffered().also {
-            val peekBuffer = it.buffer.copy()
-            println("Output Preview: " + peekBuffer.readByteString().decodeToString())
-        },
+        inputStream = process.inputStream.asSource().buffered(),
+        outputStream = process.outputStream.asSink().buffered(),
     )
 }
