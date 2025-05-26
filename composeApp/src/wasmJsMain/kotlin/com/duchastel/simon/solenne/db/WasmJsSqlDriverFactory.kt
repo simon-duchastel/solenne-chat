7cf81ca3f2910c947a7bd58b505e5cd980666440
Simon Duchastel <simon@duchastel.com>
2025-05-25 21:26:11 -0400

    correctly persist DB

--- composeApp/src/wasmJsMain/kotlin/com/duchastel/simon/solenne/db/WasmJsSqlDriverFactory.kt
+++ composeApp/src/wasmJsMain/kotlin/com/duchastel/simon/solenne/db/WasmJsSqlDriverFactory.kt
@@ -4,16 +4,20 @@ import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.worker.WebWorkerDriver
 import com.duchastel.simon.solenne.Database
 import dev.zacsweers.metro.Inject
+import kotlinx.coroutines.await
 import org.w3c.dom.Worker
+import kotlin.js.Promise
 
 @Inject
 class WasmJsSqlDriverFactory : SqlDriverFactory {
     override suspend fun createSqlDriver(): SqlDriver {
         val webWorker = createSqlWorker()
-        val driver = WebWorkerDriver(webWorker)
-        Database.Schema.create(driver).await()
-
-        return driver
+        return WebWorkerDriver(webWorker).apply {
+            val dbExists = checkIfDbExistsJs().await<JsBoolean>().toBoolean()
+            if (!dbExists) {
+                Database.Schema.create(this@apply).await()
+            }
+        }
     }
 }
 
@@ -21,4 +25,15 @@ fun createSqlWorker(): Worker = js(
     """new Worker(new URL("sqlite.worker.js", import.meta.url))"""
 )
 
-
+val checkIfDbExistsJs: () -> Promise<JsBoolean> = js("""
+  async function() {
+    try {
+      const storageManager = navigator.storage;
+      const root = await storageManager.getDirectory();
+      await root.getFileHandle("database.db", { create: false });
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+""")
